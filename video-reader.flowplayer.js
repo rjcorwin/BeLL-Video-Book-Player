fa014739b42c2ffe447d7090f6f9cbe07b639caf
rjsteinert <rj@rjsteinert.com>
2013-02-21 22:28:07 -0500

    Built out the pager but need to modify it to also update when a page changes during video play

--- video-reader.flowplayer.js
+++ video-reader.flowplayer.js
@@ -29,6 +29,7 @@ $(function() {
 
 /*
  * http://flowplayer.org/docs/api.html
+ * Flowplayer hook
  */
 flowplayer(function(api, root) {
  
@@ -38,6 +39,11 @@ flowplayer(function(api, root) {
  
   // when a video is loaded and ready to play
   }).bind("ready", function() {
+
+    /*
+     * Build out the pager
+     */
+     
     // Add Page Numbers for the cuepoints
     var currentPage = 0
     var lastPage = -1
@@ -48,6 +54,50 @@ flowplayer(function(api, root) {
     $(".fp-cuepoint" + lastPage).text("<")
     $(".fp-cuepoint" + currentPage).text("Page " + (currentPage + 1))
     $(".fp-cuepoint" + (currentPage + 1)).text(">")
+    $(".fp-cuepoint").bind("click", function() {
+
+      var destinationPage, 
+        nextPage, 
+        previousPage
+      ;
+
+      // Detect direction
+      if($(this).text() == ">") {
+        destinationPage = currentPage + 1
+      }
+      if($(this).text() == "<") {
+        destinationPage = currentPage - 1
+      }
+
+      // Depending on what page we are on will affect what is the next and previous
+      // pages.
+      if(destinationPage > lastPage) {
+        // Go back to the beginning
+        destinationPage = 0
+        nextPage = 1
+        previousPage = lastPage
+      }
+      else if (destinationPage < 0) {
+        // Go to the last page
+        destinationPage = lastPage
+        nextPage = 0
+        previousPage = lastPage - 1
+      }
+      else if (destinationPage < lastPage)  {
+        // proceed 
+        nextPage = destinationPage + 1
+        previousPage = destinationPage - 1
+      }
+
+      // Clear the pager
+      $(".fp-cuepoint").text("")
+      // Set the links
+
+      $(".fp-cuepoint" + previousPage).text("<")
+      $(".fp-cuepoint" + currentPage).text("Page " + (currentPage + 1))
+      $(".fp-cuepoint" + nextPage).text(">")
+
+    })
   });
  
 });
